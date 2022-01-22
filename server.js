af3b28d823476485295237c4ec13cd2a5e424232
Ishpreet Kaur <ikishpreet-kaur@myseneca.ca>
2022-01-22 17:01:32 -0500

    update

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ function onHttpStart() {
 
 // setup a 'route' to listen on the default url path (http://localhost)
 app.get("/", function(req,res){
-    res.sendFile(__dirname,"/views/home.html");
+    res.sendFile(path.join(__dirname,"/views/home.html"));
 });
 
 // setup another route to listen on /about
 app.get("/about", function(req,res){
-    res.sendFile(__dirname,"/views/about.html");
+    res.sendFile(path.join(__dirname,"/views/about.html"));
 });
 
 // setup http server to listen on HTTP_PORT
