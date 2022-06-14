c3102488c3049314e864a47b891639b7c0acaa5c
AnkitSharma <2698ankitsharma@gmail.com>
2022-06-14 12:05:02 +0530

    small updae

--- server.js
+++ server.js
@@ -1,13 +1,4 @@
-/*********************************************************************************
-*  WEB322 – Assignment 02
-*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
-*  (including 3rd party web sites) or distributed to other students.
-* 
-*  Name: _____Rajat Kumar_________________ Student ID: _155824204_____________ Date: _04-02-2022_______________
-*
-*  Online (Heroku) Link: ___https://obscure-oasis-06700.herokuapp.com/_____________________________________________________
-*
-********************************************************************************/ 
+ 
 
 var express = require("express");
 var app = express();
